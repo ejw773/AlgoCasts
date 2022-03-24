af07cb250f605c710a06cff6ffc82f197d3e8c11
Elijah Wilcott <ejw773@gmail.com>
2022-03-24 18:18:31 -0400

    finishes revInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let revNum = parseInt(n.toString().split('').reverse().join(''));
+  if (n < 0) {
+    return revNum * -1;
+  }
+  return revNum;
+}
 
 module.exports = reverseInt;
