890184d51483ee24cbbd0b4cc57ab6a1262fb405
Elijah Wilcott <ejw773@gmail.com>
2022-03-24 12:50:21 -0400

    completes max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charObj = {};
+  let maxNum = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (!charObj[char]) {
+      charObj[char] = 1;
+    } else {
+      charObj[char]++;
+    }
+  }
+
+  for (let char in charObj) {
+    if (charObj[char] > maxNum) {
+      maxNum = charObj[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
